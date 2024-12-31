11913c06d7621e79568c32a518a2bfc78cbd9bab
Andrew McWatters <mcwattersandrew@gmail.com>
2024-12-31 09:34:30 -0700

    Refactor LikeButton class and add MyComponent for fetching and rendering items

--- like_button.js
+++ like_button.js
@@ -1,18 +1,7 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
-// Create a class for the element
 class LikeButton extends HTMLElement {
   constructor() {
-    // Always call super first in constructor
     super();
-
     this.liked = false;
-
-    // Create a shadow root
-    /* const shadow = */ this.attachShadow({mode: 'open'});
-  }
-
-  connectCallback() {
-    this.render();
   }
 
   setLiked(value) {
@@ -20,19 +9,23 @@ class LikeButton extends HTMLElement {
     this.render();
   }
 
-  render() {
-    const shadow = this.shadowRoot;
+  connectCallback() {
+    this.attachShadow({mode: 'open'});
+    this.render();
+  }
 
-    if (this.liked) {
-      shadow.innerHTML = 'You liked this.'
+  render() {
+    const { liked, shadowRoot } = this;
+
+    if (liked) {
+      shadowRoot.innerHTML = 'You liked this.'
       return;
     }
 
-    shadow.innerHTML = `<button onclick="this.parentNode.host.setLiked(true)">
+    shadowRoot.innerHTML = `<button onclick="this.getRootNode().host.setLiked(true)">
   Like
 </button>`;
   }
 }
 
-// Define the new element
 customElements.define('like-button', LikeButton);
---
+++ my_component.js
@@ -0,0 +1,51 @@
+class MyComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.error = null;
+    this.isLoaded = false;
+    this.items = [];
+  }
+
+  connectedCallback() {
+    this.attachShadow({mode: 'open'});
+
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.isLoaded = true;
+          this.items = result;
+          this.render();
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.isLoaded = true;
+          this.error = error;
+          this.render();
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items, shadowRoot } = this;
+    if (error) {
+      shadowRoot.innerHTML = `<div>Error: {error.message}</div>`;
+    } else if (!isLoaded) {
+      shadowRoot.innerHTML = `<div>Loading...</div>`;
+    } else {
+      shadowRoot.innerHTML = (
+        `<ul>
+          ${items.map(item => (
+            `<li>
+              ${item.name} ${item.price}
+            </li>`
+          )).join('')}
+        </ul>`
+      );
+    }
+  }
+}
+
+customElements.define('my-component', MyComponent);
